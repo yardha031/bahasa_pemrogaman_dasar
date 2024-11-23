501de84e2d2552e68e5d73980854d8cb2b4e2dc9
yardha031 <yardhalasaufa4@gmail.com>
2024-11-23 04:14:24 +0000

    updatepelajaran pythonp06

--- index.py
+++ index.py
@@ -1,1 +1,23 @@
+x = int(input("Masukkan sebuah bilangan : "))
+if x < 200:
+    keterangan = " : Nilai kurang dari 200"
+    print(x, keterangan)
+    if x == 150:
+        keterangan = " : Sama dengan 150"
+        print(x, keterangan)
+    elif x == 100: 
+        keterangan = " : Sama dengan 100"
+        print(x, keterangan)
+    elif x == 50: 
+        keterangan = " : Sama dengan 50"
+        print(x, keterangan)
+    elif x < 50: 
+        keterangan = " : kurang dari 50"
+        print(x, keterangan)
+else:
+    keterangan = "Nilai yang dimasukan salah"
+    print(keterangan)
+
+print("selesai")
+
 
